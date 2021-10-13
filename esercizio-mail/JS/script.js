fc245d7bae4e0d82550bb808f8858550764cccf4
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-13 18:12:10 +0200

    problema con l'output del controllo email

--- esercizio-mail/JS/script.js
+++ esercizio-mail/JS/script.js
@@ -3,31 +3,27 @@
 // CONTROLLO SE NELLA LISTA HO LA MAIL CHE HA INSERITO  
 // STAMPO OUTPUT CON ESITO DEL CONTROLLO
 
-let promptEmail = prompt("Inserisci la tua e-mail");
 const listaMail = ["pippo@gmail.com", "boolean@gmail.com", "dipende@gmail.com", "florian@gmail.com", "clelia@gmail.com", "flavio@gmail.com", "alessandro@gmail.com"];
-
-/* console.log(listaMail); */
-
+let promptEmail = prompt("Inserisci la tua e-mail");
+console.log(listaMail);
+/* let mailTrue = true;
+let mailFalse = false; */
 
 for (i = 0; i < listaMail.length; i++) {
     listaMail[i];
-    console.log(" posizione " + i + " Elemento: " + listaMail[i]);
     const mailCorrente = listaMail[i];
-
-
+    console.log(" posizione " + i + ": " + listaMail[i]);
 
     if (promptEmail === mailCorrente) {
         console.log("email corretta");
-        /* alert("Benvenuto"); */
-    }
 
-    if (promptEmail !== mailCorrente) {
+    } if (promptEmail !== mailCorrente) {
         console.log("email errata");
-        /* alert("errore"); */
+
     }
-
-
 }
+
+
 
 // NON RIESCO A STAMPARE SOLO UN RISULATO NEL CONSOLE.LOG
 
